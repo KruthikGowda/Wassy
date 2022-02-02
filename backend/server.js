758e155ca92518a9b03fac89a7b9ee5c2cc0f5ea
Kruthik Gowda <prkruthikgowda@gmail.com>
2022-02-02 20:19:49 +0530

    coomit

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
-const express = require("express");
-const products = require("./data/products");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/mongoDb.js";
+import colors from "colors";
+import productRouter from "./routes/productRouter.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running..........");
 });
 
-app.get("/v1/products", (req, res) => {
-  res.json(products);
-});
+app.use("/v1/products", productRouter);
 
-app.get("/v1/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+const middlewares = [notFound, errorHandler];
+
+app.use(middlewares);
 
 app.listen(
   port,
-  console.log("Server is running on port: http://localhost:" + port)
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port: http://localhost:${port}`
+      .yellow.bold
+  )
 );
